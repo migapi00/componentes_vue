6e9ed580f9c934cfb0ca069772bcac2d5aaa43ce
migapi00 <migapi00@gmail.com>
2023-05-03 11:12:58 +0200

    computed

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,60 +1,24 @@
-<!-- <template>
-    <div>{{ obj }}</div>
+<template>
+    <!-- <div>{{firstName }} {{ lastName }}</div> -->
+    <div>{{ fullName }} </div>
 </template>
-
+  
 <script>
-// import { ref } from 'vue';
-import { reactive, watch } from 'vue';
+import { ref, computed } from "vue"
 
 export default {
     setup() {
-        // const counter = ref(0);
-        const obj = reactive({counter:0});
-        // setInterval(() => { counter.value++}, 500);
+        const firstName = ref("Luis");
+        const lastName = ref("Garcia");
 
-        setInterval(() => { obj.counter++ }, 500);
-
-        watch(
-            () => obj.counter,
-            (valor, anterior) => {
-                console.log(valor, anterior);
-            }
-        );
+        const fullName = computed(
+            () => {
+                return `${firstName.value} ${lastName.value}`;
+            });
 
         return {
-            obj,
-        }
-    },
-}
-</script>  -->
-
-<template>
-    <h1>{{ text }}</h1>
-    <h2>{{ counter }}</h2>
-  </template>
-  
-  <script>
-  import { ref, watch } from "vue"
-  
-  export default {
-    setup() {
-      const text = ref('Hello World')
-      const counter = ref(0)
-  
-      text.value = 'Hello Vue!'
-  
-      setInterval(() => {
-        counter.value++
-      }, 1500)
-  
-      watch(counter, (newValue, oldValue) => {
-        console.log(`old: ${oldValue} - new: ${newValue}`)
-      })
-  
-      return {
-        text,
-        counter,
-      }
+            fullName,
+        };
     }
-  }
-  </script>
+}
+</script>
